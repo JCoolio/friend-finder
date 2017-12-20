cc273c958e8a6f3c728203192e5db18014a5d2d3
Jacob Cooley <TheEnigmaMachine@Jacobs-MacBook-Pro.local>
2017-12-20 14:41:31 -0600

    final

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+
+var friendList = [
+  {
+    name: 'Jacob Cooley',
+    profilePic: 'https://vignette4.wikia.nocookie.net/ironman/images/9/9d/Tumblr_l1iotoYo541qbn8c7.jpg/revision/latest?cb=20131120195052',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
