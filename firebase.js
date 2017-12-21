abd355b847264ccb54d905df878543963bac17a3
JCoolio <30607987+JCoolio@users.noreply.github.com>
2017-12-20 21:11:48 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+  var config = {
+    apiKey: "AIzaSyCx3HbA0QWxFOu2QjqH0Min3woUw6z-Keo",
+    authDomain: "friend-finder-b53b2.firebaseapp.com",
+    databaseURL: "https://friend-finder-b53b2.firebaseio.com",
+    projectId: "friend-finder-b53b2",
+    storageBucket: "",
+    messagingSenderId: "10303371225"
+  };
+  firebase.initializeApp(config);
+
+
